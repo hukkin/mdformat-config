145182c1a58da0615bc40fb5405b17bd3cd8f3cd
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-09-21 16:51:54 +0300

    Bump version: 0.0.0 → 0.1.0

--- mdformat_config/__init__.py
+++ mdformat_config/__init__.py
@@ -3,7 +3,7 @@ import json
 import toml
 
 
-__version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 
 def format_json(unformatted: str) -> str:
